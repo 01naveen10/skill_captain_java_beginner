5cadb68887fa64e7a6afc1415cad3449ab346ed2
naveen <rns125naveenvrgos@gmail.com>
2023-06-23 23:02:28 +0530

    day 2

---
+++ beginner/src/day2/secondCode.java
@@ -0,0 +1,15 @@
+package day2;
+
+public class secondCode {
+    public static void main(String[] args){
+        byte v1 = 12;
+        short v2 = 453;
+        int v3 = 4232;
+        long v4 = 50003L;
+        float v5 = 23.3f;
+        double v6 = 567.32d;
+
+        System.out.println(v1 + "\n" + v2+ "\n" + v3 + "\n" + v4 + "\n" + v5 + "\n" + v6 +"\n");
+
+    }
+}
